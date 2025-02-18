9e51a9c8b0adf4417b10c069c2f99886531c0e8d
MUSTAFA ÇAY <mcay51@gmail.com>
2025-02-19 01:35:15 +0300

    Global exception handler sınıfı güncellendi

--- spring-transaction-demo/src/main/java/tr/com/mustafacay/transaction/exception/GlobalExceptionHandler.java
+++ spring-transaction-demo/src/main/java/tr/com/mustafacay/transaction/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tr.com.mustafacay.transaction.dto.ErrorResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
